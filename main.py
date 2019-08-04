4b8e71090d861e9f2b373ddccfe83ff58ca8114a
Christian Diaz <48182689+chrisd149@users.noreply.github.com>
2019-08-04 03:49:03 -0400

    added buttons template and updated ai
    updated ai to be less aggressive, added a template for buttons to clean up code, and added a helper toon that will be updated later

--- main.py
+++ main.py
@@ -4,55 +4,55 @@ from direct.showbase.DirectObject import DirectObject
 from direct.task import Task
 from direct.actor.Actor import Actor
 from panda3d.ai import *
-from direct.interval.IntervalGlobal import *
-from direct.gui.OnscreenText import OnscreenText
 from direct.gui.DirectGui import *
 
-def addInstructions(pos, txt):
+
+def addOnscreenText(x, y, txt):
     return OnscreenText(text=txt,
                         style=1,
-                        fg=(1,1,1,1),
-                        pos=(-1.3, pos),
+                        fg=(1, 1, 1, 1),
+                        pos=(x, y),
                         align=TextNode.ALeft,
-                        scale = .05,
+                        scale=.05,
                         wordwrap=15)
+
+
+def addButtons(x, y, z, txt, command):
+    return DirectButton(text=txt,
+                        pos=(x, y, z),
+                        scale=.05,
+                        command=command
+                        )
+
 
 class Scene(DirectObject):
     def __init__(self):
 
-        # instructions
-        addInstructions(-.5, "Spam the buttons to make the toon pursure the cog or wander around")
+        # onscreentext
+        addOnscreenText(-1.75, .9, "Control the helper toon")
+        # addOnscreenText(1, .9, "Spam the buttons to make the toon pursure the cog or wander around")
+
+        #
+        addButtons(1.5, 0, .5, "Pursure Cog", self.pursure_cog)
+        addButtons(1.5, 0, .6, "Wander", self.wander)
 
         base.cam.setPosHpr((0, -25, 5), (0, -5, 0))
 
         self.load_models()
         self.cog_ai()
 
-        self.pursure = DirectButton(text="Pursure Cog",
-                                    pos=(1.5, .5, .5),
-                                    command=self.pursure_cog,
-                                    scale=.05
-                                    )
-        self.wander = DirectButton(text="Wander",
-                                    pos=(1.5, 1, .9),
-                                    command=self.wander,
-                                    scale=.05
-                                    )
-
     def load_models(self):
 
         # toon starting pos
         self.toon_start = Point3(0, -25, 0)
+        self.helper_start= Point3(5, -35, 0)
 
         # toon models
-        self.legs = Actor('phase_3\models\char/tt_a_chr_dgs_shorts_legs_1000.bam')
-        self.torso = Actor('phase_3\models\char/tt_a_chr_dgm_shorts_torso_1000.bam')
-        self.head = Actor('phase_3\models\char/tt_a_chr_dgm_skirt_head_1000.bam')
+        self.toon_legs = Actor('phase_3\models\char/tt_a_chr_dgs_shorts_legs_1000.bam')
+        self.toon_torso = Actor('phase_3\models\char/tt_a_chr_dgm_shorts_torso_1000.bam')
+        self.toon_head = Actor('phase_3\models\char/tt_a_chr_dgm_skirt_head_1000.bam')
 
-        self.botHead = self.head.find('**/head-front')
-        self.tophead = self.head.find('**head')
-
-        self.toon = Actor({'legs': self.legs, 'torso': self.torso, 'head': self.head},
+        self.toon = Actor({'legs': self.toon_legs, 'torso': self.toon_torso, 'head': self.toon_head},
                           {'legs': {'Static': 'phase_3\models\char/tt_a_chr_dgs_shorts_legs_1000.bam',
                                     'Idle': 'phase_3/models/char/tt_a_chr_dgs_shorts_legs_neutral.bam',
                                     'Walk': 'phase_3.5/models/char/tt_a_chr_dgs_shorts_legs_walk.bam',
@@ -74,6 +74,36 @@ class Scene(DirectObject):
         self.toon.attach('head', 'torso', 'def_head')
         self.toon.reparentTo(render)
         self.toon.setPos(self.toon_start)
+        self.toon.loop('Idle')
+
+        # toon models
+        self.helper_legs = Actor('phase_3\models\char/tt_a_chr_dgl_shorts_legs_1000.bam')
+        self.helper_torso = Actor('phase_3\models\char/tt_a_chr_dgs_shorts_torso_1000.bam')
+        self.helper_head = Actor('phase_3\models\char/tt_a_chr_dgm_skirt_head_1000.bam')
+
+        self.helper = Actor({'legs': self.helper_legs, 'torso': self.helper_torso, 'head': self.helper_head},
+                            {'legs': {'Static': 'phase_3\models\char/tt_a_chr_dgm_shorts_legs_1000.bam',
+                                      'Idle': 'phase_3/models/char/tt_a_chr_dgl_shorts_legs_neutral.bam',
+                                      'Walk': 'phase_3.5/models/char/tt_a_chr_dgs_shorts_legs_walk.bam',
+                                      'Wave': 'phase_3.5/models/char/tt_a_chr_dgs_shorts_legs_wave.bam',
+                                      'Run': 'phase_3/models/char/tt_a_chr_dgs_shorts_legs_run.bam', },
+
+                             'torso': {'Static': 'phase_3\models\char/tt_a_chr_dgm_shorts_torso_1000.bam',
+                                       'Idle': 'phase_3/models/char/tt_a_chr_dgs_shorts_torso_neutral.bam',
+                                       'Walk': 'phase_3.5/models/char/tt_a_chr_dgm_shorts_torso_walk.bam',
+                                       'Wave': 'phase_3.5/models/char/tt_a_chr_dgm_shorts_torso_wave.bam',
+                                       'Run': 'phase_3/models/char/tt_a_chr_dgm_shorts_torso_run.bam', },
+
+                             'head': {'Idle': 'phase_3/models/char/tt_a_chr_dgm_skirt_head_neutral.bam',
+                                      'Walk': 'phase_3.5/models/char/tt_a_chr_dgm_skirt_head_walk.bam',
+                                      'Wave': 'phase_3.5/models/char/tt_a_chr_dgm_skirt_head_wave.bam',
+                                      'Run': 'phase_3/models/char/tt_a_chr_dgm_skirt_head_run.bam', }})
+
+        self.helper.attach('torso', 'legs', 'joint_hips')
+        self.helper.attach('head', 'torso', 'def_head')
+        self.helper.reparentTo(render)
+        self.helper.setPos(self.helper_start)
+        self.helper.loop('Idle')
 
         # cog actor
         self.cog = Actor(Actor('phase_3.5\models\char\suitA-mod.bam',
@@ -93,8 +123,7 @@ class Scene(DirectObject):
 
         self.AIbehaviors.pursue(self.cog, 1.0)
 
-        # AI World update
-        taskMgr.add(self.AIUpdate, "AIUpdate")
+        self.toon.loop('Run')
 
     def wander(self):
         # wanders around a point
@@ -102,10 +131,9 @@ class Scene(DirectObject):
         self.AIworld.addAiChar(self.AIchar)
         self.AIbehaviors = self.AIchar.getAiBehaviors()
 
-        self.AIbehaviors.wander(10, 10, 10, 0.5)
+        self.AIbehaviors.wander(10, 10, 10, 0.75)
 
-        # AI World update
-        taskMgr.add(self.AIUpdate, "AIUpdate")
+        self.toon.loop('Run')
 
     def cog_ai(self):
         # renders AI world
@@ -127,7 +155,7 @@ class Scene(DirectObject):
     # Updates all AI in self.AIworld
     def AIUpdate(self, task):
         self.AIworld.update()
-        return Task.cont
+        return task.cont
     # this causes the AI to get a bit wacky when too many
     # AI updates are made
 
